b5d6e571944c10ac9df44ce2a88e9d090370e0cd
Ieuan Walker <ieuan.walker007@gmail.com>
2024-08-22 16:30:28 +0100

    added test

--- TinyInsights.TestApp/NewPage.xaml.cs
+++ TinyInsights.TestApp/NewPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace TinyInsights.TestApp;
 
 public partial class NewPage : ContentPage
@@ -5,5 +7,22 @@ public partial class NewPage : ContentPage
 	public NewPage()
 	{
 		InitializeComponent();
-	}
+
+        Appearing += (_,_) =>
+        {
+            Debug.WriteLine("NewPage Appearing event");
+        };
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        Debug.WriteLine("NewPage OnAppearing override");
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new NewPage1());
+    }
 }
