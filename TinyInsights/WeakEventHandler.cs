58853cdf34aaaf30aa0fbd885cac20c857110dcd
Daniel Hindrikes <daniel@hindrikes.se>
2024-09-02 13:47:25 +0200

    Added a weak event handler

---
+++ TinyInsights/WeakEventHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace TinyInsights;
+
+internal class WeakEventHandler<TEventArgs>
+{
+    private readonly WeakReference<EventHandler<TEventArgs>> _weakHandler;
+
+    public WeakEventHandler(EventHandler<TEventArgs> handler)
+    {
+        _weakHandler = new WeakReference<EventHandler<TEventArgs>>(handler);
+    }
+
+    public void Handler(object sender, TEventArgs e)
+    {
+        if (_weakHandler.TryGetTarget(out var handler))
+        {
+            handler(sender, e);
+        }
+    }
+}
+
