9dd8214aca5d1df3ef39fa5a917fe912093618ac
Daniel Hindrikes <daniel@hindrikes.se>
2025-03-08 18:49:38 +0100

    Created channel

---
+++ TinyInsights/Channels/Offline/OfflineTelemetryChannel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+
+namespace TinyInsights.Channels.Offline;
+
+public class OfflineTelemetryChannel : ITelemetryChannel
+{
+    public bool? DeveloperMode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string EndpointAddress { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Flush()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Send(ITelemetry item)
+    {
+        throw new NotImplementedException();
+    }
+}
