ae0434349938f0ce72c2e9884b5685614f8fadf0
Daniel Hindrikes <daniel@hindrikes.se>
2025-06-24 08:22:12 +0200

    time range

--- TinyInsights.Web/Pages/Query.razor.cs
+++ TinyInsights.Web/Pages/Query.razor.cs
@@ -9,4 +9,19 @@ public partial class Query
         {"Crashes per day", "exceptions | where customDimensions.IsCrash == true | summarize count() by bin(timestamp, 1d)" },
         {"Users per day", "customEvents | summarize dcount(user_Id) by bin(timestamp, 1d)" }
     };
+
+    private const string CustomTimeRangeValue = "custom";
+
+    private Dictionary<string, string> timeRanges = new()
+    {
+        {"Last hours", "1h" },
+        {"Last 4 hours", "4h" },
+        {"Last 12 hours", "12h" },
+        {"Last 24 hours", "24h" },
+        {"Last 48 hours", "48h" },
+        { "Last 3 days", "3d" },
+        { "Last 7 days", "7d" },
+        { "Last 30 days", "30d" },
+        {"Custom", CustomTimeRangeValue }
+    };
 }
